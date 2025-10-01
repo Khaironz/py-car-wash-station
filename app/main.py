af4f41bc33bbaa31fa93701e0b3c53a22b597659
Khaironz <roberto.tec27@gmail.com>
2025-10-01 10:28:37 -0300

    Car, CarWashStation class created 2

--- app/main.py
+++ app/main.py
@@ -13,3 +13,32 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
+    def calculate_washing_price(self, car: Car) -> float:
+        """Calcula o preço de lavagem de um carro sem lavá-lo."""
+        price = (car.comfort_class *
+                 (self.clean_power - car.clean_mark) *
+                 self.average_rating /
+                 self.distance_from_city_center)
+        return round(price, 1)
+
+    def wash_single_car(self, car: Car) -> float:
+        """Lava o carro, se possível, e retorna o preço da lavagem."""
+        if car.clean_mark < self.clean_power:
+            price = self.calculate_washing_price(car)
+            car.clean_mark = self.clean_power
+            return price
+        return 0.0
+
+    def serve_cars(self, cars: list[Car]) -> float:
+        """Atende uma lista de carros, lavando os possíveis e somando a renda."""
+        total_income = 0.0
+        for car in cars:
+            total_income += self.wash_single_car(car)
+        return round(total_income, 1)
+
+    def rate_service(self, new_rating: int):
+        """Adiciona uma nova avaliação e recalcula a média."""
+        total_score = self.average_rating * self.count_of_ratings
+        total_score += new_rating
+        self.count_of_ratings += 1
+        self.average_rating = round(total_score / self.count_of_ratings, 1)
